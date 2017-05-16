ab61703c011c0cc59b6e404ceeb8730ef4e5d748
Zane M. Kolnik <zane@zanematthew.com>
2017-05-16 16:35:54 -0400

    Test DB
    Added support for a testing Database via `config/database.php`, and `phpunit.xml`.

--- config/database.php
+++ config/database.php
@@ -54,6 +54,20 @@ return [
             'engine' => null,
         ],
 
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_TEST'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),

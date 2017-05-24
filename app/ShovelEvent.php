6e295049d530c17602ca2129a6f32947bf8a2fd3
Zane M. Kolnik <zane@zanematthew.com>
2017-05-24 16:33:37 -0400

    Refactoring `ShovelEvents`, adding unit test.

--- app/ShovelEvent.php
+++ app/ShovelEvent.php
@@ -2,182 +2,26 @@
 
 namespace App;
 
-use Goutte;
-
-class ShovelEvent
+class ShovelEvent extends Shovel
 {
-
     use ShovelTrait;
-    use ShovelParserTrait;
 
-    protected $url = 'https://www.usabmx.com/site';
-
-    public $eventTypes = [
-        'National' => [
-            'section_id' => 228,
-        ],
-        'Earned Double' => [
-            'section_id' => 95,
-        ],
-        'Gold Cup' => [
-            'section_id' => 24,
-        ],
-        'Race for Life' => [
-            'section_id' => 19,
-        ],
-        'State' => [
-            'section_id' => 23,
-        ],
-    ];
-
-    private $slug = '/bmx_races';
-
-    /**
-     * Given an event ID retrieve the Type
-     *
-     * @param  [type] $id [description]
-     * @return [type]     [description]
-     */
-    public function getEventTypeFromId($eventId)
+    public function idFromShareLinks()
     {
-        return array_keys($this->eventTypes)[array_search(
-            $eventId,
-            array_column(
-                $this->eventTypes,
-                'section_id'
-            )
-        )];
+        $uri = $this->filter('.share_race a')->eq(0)->attr('href');
+        return current(explode('?', last(explode('/', $uri))));
     }
 
-    /**
-     * Build the HTTP query params
-     *
-     * @param  string $type The event Type
-     * @return string       URL encoded parameters
-     */
-    public function buildUrl($type, $page = null, $year = null)
+    public function title()
     {
-        $sectionId = $this->eventTypes[$type]['section_id'];
-
-        switch ($sectionId) {
-            case 228:
-                $additionalParams = ['category' => strtoupper($type)];
-                break;
-            case 95:
-                $additionalParams = ['series_race_type' => $type];
-                break;
-            case 24:
-                $additionalParams = ['goldcup' => 1];
-                break;
-            case 19:
-                $additionalParams = ['series_race_type' => $type];
-                break;
-            case 23:
-                $additionalParams = ['filter_state' => 1];
-                break;
-            default:
-                $additionalParams = null;
-                break;
-        }
-
-        $pastOnly = 1;
-        $yearFix  = $year;
-
-        if ($year == 'Upcoming') {
-            $pastOnly = 0;
-            $yearFix  = 'UPCOMING';
-        }
-
-        return $this->eventUrl() . '?' . http_build_query(array_merge([
-            'section_id' => $sectionId,
-            'year'       => $yearFix,
-            'past_only'  => $pastOnly,
-            'page'       => empty($page) ? 1 : $page,
-        ], $additionalParams));
+        return $this->filter('#event_title')->text();
     }
 
-    /**
-     * Gets start date
-     *
-     * @param  [type] $date [description]
-     * @return [type]       [description]
-     */
-    public function getStartDate($date)
+    // Most events just say "There is no description for this race."
+    public function description(){}
+
+    public function venueId()
     {
-        $date = $this->date($date);
-        if (is_array($date)) {
-            $date = current($date);
-        }
-        return $date;
-    }
-
-    /**
-     * Gets the end date
-     *
-     * @param  [type] $date [description]
-     * @return [type]       [description]
-     */
-    public function getEndDate($date)
-    {
-        $date = $this->date($date);
-        if (is_array($date)) {
-            $date = last($date);
-        }
-
-        return $date;
-    }
-
-    /**
-     * Get events
-     *
-     * @param  string $url [description]
-     * @return [type]       [description]
-     */
-    public function getEvents($url = null)
-    {
-
-        $crawler = Goutte::request('GET', $url);
-        $events = $crawler->filter('.event_details')->each(
-            function ($node) {
-
-                $title     = null;
-                $startDate = null;
-                $endDate   = null;
-                $trackId   = null;
-                $eventId   = null;
-
-                if ($node->filter('.event_title')->count()) {
-                    $title = $this->cleanText($node->filter('.event_title')->text());
-                    $eventId = $this->eventIdFromUri($node->filter('.event_title a')->attr('href'));
-                }
-
-                if ($node->filter('.event_date')->count()) {
-                    $date = $this->cleanText($node->filter('.event_date')->text());
-                    $startDate = $this->getStartDate($date);
-                    $endDate = $this->getEndDate($date);
-                }
-
-                if ($node->filter('.event_location a')->count()) {
-                    $trackId = $this->venueId($node->filter('.event_location a')->first()->attr('href'));
-                }
-
-                return [
-                    'id'         => $eventId,
-                    'title'      => $title,
-                    'start_date' => $startDate,
-                    'end_date'   => $endDate,
-                    'track' => [
-                        'id'   => $trackId
-                    ]
-                ];
-            }
-        );
-
-        return $events;
-    }
-
-    public function eventUrl()
-    {
-        return $this->url . $this->slug;
+        return last(explode('/', $this->filter('#venue_title a')->eq(0)->attr('href')));
     }
 }
--- tests/Unit/ShovelEventTest.php
+++ tests/Unit/ShovelEventTest.php
@@ -3,122 +3,51 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use Goutte\Client as GoutteClient;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 
 use App\ShovelEvent;
 
 class ShovelEventTest extends TestCase
 {
-
     public function setUp()
     {
         parent::setUp();
-        $this->event = new ShovelEvent;
-        $this->url   = 'https://www.usabmx.com/site/bmx_races';
+
+        $eventHtml = file_get_contents(base_path('tests/Unit/data/event.html'));
+
+        $mock = new MockHandler([
+            new Response(200, ['Content-type' => 'text/html'], $eventHtml),
+        ]);
+
+        $httpClient = new GuzzleClient([
+            'handler' => HandlerStack::create($mock),
+        ]);
+
+        $goutte = new GoutteClient();
+        $goutte->setClient($httpClient);
+
+        $event = new ShovelEvent('http://example.com');
+        $event->setClient($goutte);
+        $this->event = $event;
     }
 
-    /**
-     * Ensures that we can parse a start date, and an end date
-     * from an entry as [date - date]
-     *
-     * @return void
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testDate()
+    public function testIdFromShareLinks()
     {
-        $this->assertEquals('July 01, 2016', $this->event->getStartDate(' July 01, 2016 - July 03, 2016'));
-        $this->assertEquals('July 03, 2016', $this->event->getEndDate("\n\n\t  July 01, 2016 - July 03, 2016\t\t\n"));
-        $this->assertEquals('July 01, 2016', $this->event->getStartDate(' July 01, 2016'));
-        $this->assertEquals('July 03, 2016', $this->event->getEndDate('  July 03, 2016'));
+        $this->assertEquals(334979, $this->event->idFromShareLinks());
     }
 
-    /**
-     * Most important test, verifies that we can build specific query params.
-     *
-     * @return void
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlNational()
+    public function testTitle()
     {
-        $this->assertEquals(
-            $this->url.'?section_id=228&year=UPCOMING&past_only=0&page=1&category=NATIONAL',
-            $this->event->buildUrl('National', 1, 'Upcoming')
-        );
+        $this->assertEquals('State Race Double', $this->event->title());
     }
 
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlNationalPast()
+    public function testVenueId()
     {
-        $this->assertEquals(
-            $this->url.'?section_id=228&year=2005&past_only=1&page=1&category=NATIONAL',
-            $this->event->buildUrl('National', 1, 2005)
-        );
-    }
-
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlState()
-    {
-        $this->assertEquals(
-            $this->url.'?section_id=23&year=UPCOMING&past_only=0&page=1&filter_state=1',
-            $this->event->buildUrl('State', 1, 'Upcoming')
-        );
-    }
-
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlEarnedDouble()
-    {
-        $this->assertEquals(
-            $this->url.'?section_id=95&year=UPCOMING&past_only=0&page=1&series_race_type=Earned+Double',
-            $this->event->buildUrl('Earned Double', 1, 'Upcoming')
-        );
-    }
-
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlGoldCup()
-    {
-        $this->assertEquals(
-            $this->url.'?section_id=24&year=UPCOMING&past_only=0&page=1&goldcup=1',
-            $this->event->buildUrl('Gold Cup', 1, 'Upcoming')
-        );
-    }
-
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildUrlRaceForLife()
-    {
-        $this->assertEquals(
-            $this->url.'?section_id=19&year=UPCOMING&past_only=0&page=1&series_race_type=Race+for+Life',
-            $this->event->buildUrl('Race for Life', 1, 'Upcoming')
-        );
-    }
-
-    /**
-     * [testUrlNationalPast description]
-     * @return [type] [description]
-     * @SuppressWarnings(PHPMD)
-     */
-    public function testBuildEventUrl()
-    {
-        $this->assertEquals($this->url.'', $this->event->eventUrl());
+        $this->assertEquals(196, $this->event->venueId());
     }
 }

209a0f9e7247cec276d4dea0b7d8204c510cd6c8
Zane M. Kolnik <zane@zanematthew.com>
2017-05-31 22:01:12 -0400

    Removed old command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Old
-        Commands\ShovelEventsIndexToJson::class,
 
         // Single
         Commands\ShovelSingleVenueCommand::class,

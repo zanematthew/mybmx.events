78ece4cfb9bb57f89434e3dfa70054d49afe1459
Zane M. Kolnik <zane@zanematthew.com>
2017-06-08 07:54:44 -0400

    Using route grouping.

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ Route::get('/event/{id}/{slug?}', function ($id, $slug = '') {
     return response()->json(App\Event::with('venue.city')->where('id', $id)->first());
 })->where(['id' => '[0-9]+', 'slug' => '[a-z0-9-]+'])->name('event.single');
 
-Route::get('/events/', function () {
-    return App\Event::with('venue.city.states')->paginate(10);
-})->name('events');
+Route::group(['prefix' => 'api/events'], function () {
+    Route::get('/', function () {
+        return App\Event::with('venue.city.states')->paginate(10);
+    })->name('events');
 
-Route::get('/events/{state?}', function ($state = '') {
-    return App\Event::with('venue.city.states')->whereHas('venue.city.states', function ($query) use ($state) {
-        $query->where('abbr', strtoupper($state));
-    })->paginate(10);
-})->name('events.state');
+    Route::get('/{state?}', function ($state = '') {
+        return App\Event::with('venue.city.states')->whereHas('venue.city.states', function ($query) use ($state) {
+            $query->where('abbr', strtoupper($state));
+        })->paginate(10);
+    })->name('events.state');
+});

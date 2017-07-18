cea5d42379150289df3a32d6a7820cccf4232f6b
Zane M. Kolnik <zane@zanematthew.com>
2017-07-18 07:56:35 -0400

    items no longer used as a property.

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="masthead row">
     <div class="top row">
-        <primary-nav :items="primaryNav"></primary-nav>
+        <primary-nav></primary-nav>
     </div>
 </div>
 <router-view></router-view>

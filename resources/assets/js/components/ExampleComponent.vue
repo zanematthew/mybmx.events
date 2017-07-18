76f03467822f4de38744419925b8af4654116178
Zane M. Kolnik <zane@zanematthew.com>
2017-07-17 21:32:59 -0400

    Removing example component.

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    mounted() {
+        axios.get('/api/user').then(response => {
+            console.log('Success');
+            console.log(response.data);
+        }).catch(error => {
+            console.log('Error');
+            console.log(error);
+        });
+    }
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
 <div class="container" id="app">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <example-component></example-component>
             <passport-clients></passport-clients>
             <passport-authorized-clients></passport-authorized-clients>
             <passport-personal-access-tokens></passport-personal-access-tokens>

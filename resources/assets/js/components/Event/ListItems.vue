f6ac46d75746995452f3a757c674ca4c72678e8d
Zane M. Kolnik <zane@zanematthew.com>
2017-08-18 13:58:38 -0400

    Cleaning up markup, adjusting landing URL.

--- resources/assets/js/components/Event/ListItems.vue
+++ resources/assets/js/components/Event/ListItems.vue
@@ -1,29 +1,21 @@
 <template>
 <div>
-  <!-- <event-mini> -->
   <div class="content row is-item" v-for="event in events">
-    <div class="event-mini">
-      <div class="grid is-10">
-        <schedule-add-to-master :event="event"></schedule-add-to-master>
+    <schedule-add-to-master class="grid is-10" :event="event"></schedule-add-to-master>
+    <div class="grid is-90">
+      <div class="title">
+        <router-link :to="{ name: 'event-single', params: { id: event.id, slug: event.slug } }">
+          {{ event.title }}
+        </router-link>
+        <router-link :to="{ name: 'action-main', params: { id: event.id, landingUrl: landingUrl } }" class="align-right">
+          <icon name="ellipsis-h"></icon>
+        </router-link>
       </div>
-      <div class="grid is-90">
-        <div class="title">
-          <router-link :to="{ name: 'event-single', params: { id: event.id, slug: event.slug } }">
-            {{ event.title }}
-          </router-link>
-          <router-link :to="{ name: 'action-main', params: { id: event.id } }" class="align-right">
-            <icon name="ellipsis-h"></icon>
-          </router-link>
-        </div>
-        <div class="body">
-          <div>
-            {{ event.venue.name }} &bull; {{ event.venue.city.name }}<span v-if="event.venue.city.states">, {{ event.venue.city.states[0].abbr }}</span>
-          </div>
-        </div>
+      <div class="body">
+        {{ event.venue.name }} &bull; {{ event.venue.city.name }}<span v-if="event.venue.city.states">, {{ event.venue.city.states[0].abbr }}</span>
       </div>
     </div>
   </div>
-  <!-- <event-mini> -->
 </div>
 </template>
 <script>
@@ -37,9 +29,20 @@ export default {
     'schedule-add-to-master': ScheduleAddToMaster
   },
   props: ['events'],
+  data() {
+    return {
+      landingUrl: ''
+    }
+  },
   mounted() {
     if (!_.isEmpty(window.laravel.user)) {
       this.$store.dispatch('fetchAllScheduledEventIds');
+    }
+    this.landingUrl = `${window.location.origin}${window.location.pathname}${window.location.search}`;
+  },
+  watch: {
+    '$route' (to, from) {
+      this.landingUrl = `${window.location.origin}${window.location.pathname}${window.location.search}`;
     }
   }
 }

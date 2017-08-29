ea97824fc00b063fe4316d8945960dbdaff94e8c
Zane M. Kolnik <zane@zanematthew.com>
2017-08-29 14:37:42 -0400

    Adding no events message.

--- resources/assets/js/components/Event/Tabs.vue
+++ resources/assets/js/components/Event/Tabs.vue
@@ -8,9 +8,17 @@
       }" class="nav-item">{{ item.title }}</router-link>
     </span>
   </div>
+
+  <div v-if="events.total === 0" class="row is-item">
+    <div class="grid is-100">
+      <p>There are <strong>No events</strong> at this time. Please refer to the Venue website.</p>
+    </div>
+  </div>
+
   <div class="row is-item" v-for="event in events.data">
     <action-bar :event="event"></action-bar>
   </div>
+
   <pager :data="events"></pager>
 </div>
 </template>

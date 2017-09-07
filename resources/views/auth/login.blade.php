32c496c31bf63702d6c364a36838cdd4fcf6fd75
Zane M. Kolnik <zane@zanematthew.com>
2017-09-07 16:57:40 -0400

    Login register blade files.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,35 @@
 @extends('layouts.app')
-
 @section('title', '| Login')
-@section('body_class', 'login')
 
 @section('content')
-<div class="box">
-    <center>Logo Here</center>
-    <p><a href="/redirect/facebook">Login with Facebook</a></p>
-    <div class="main">
-        <form class="form" role="form" method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
-            <div class="row">
-            @if ($errors->has('email'))
-                {{ $errors->first('email') }}
-            @endif
-            @if ($errors->has('password'))
-                {{ $errors->first('password') }}
-            @endif
-            </div>
+<div class="grid is-100">
+    <form class="form" role="form" method="POST" action="{{ route('login') }}">
+        {{ csrf_field() }}
 
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="envelope"></icon></span>
-                <input id="email" type="email" name="email" placeholder="E-mail..." value="{{ old('email') }}" required autofocus>
-            </div>
-
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="lock"></icon></span>
-                <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-            </div>
-            <div class="row">
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-            </div>
-
+        @if ($errors->has('email'))
+            {{ $errors->first('email') }}
+        @endif
+        @if ($errors->has('password'))
+            {{ $errors->first('password') }}
+        @endif
+        <div class="form-item">
+            <a href="/redirect/facebook" class="login-with-facebook-container">
+                <icon name="facebook-square"></icon> Login with Facebook
+            </a>
+        </div>
+        <div class="form-item">
+            <input id="email" type="email" name="email" placeholder="E-mail..." value="{{ old('email') }}" required autofocus>
+        </div>
+        <div class="form-item">
+            <input id="password" type="password" class="form-control" name="password" placeholder="Password" required><input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+        </div>
+        <div class="form-item">
             <button type="submit" class="btn-primary">Login</button>
-        </form>
-    </div>
-    <ul class="mini-nav nav">
-        <li><a href="{{ route('register') }}">Register</a></li>
-        <li><a href="{{ route('password.request') }}">Forgot Your Password?</a></li>
-    </ul>
+        </div>
+
+        <div class="form-item align-center">
+            <a href="{{ route('password.request') }}">Forgot Your Password?</a> | <a href="{{ route('register') }}">Register</a>
+        </div>
+    </form>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,46 @@
 @extends('layouts.app')
 
 @section('title', '| Register')
-@section('body_class', 'login')
 
 @section('content')
-<div class="box">
-    <center>Logo Here</center>
-    <p><a href="/redirect/facebook">Login with Facebook</a></p>
-    <div class="main">
-        <form class="form" role="form" method="POST" action="{{ route('register') }}">
-            {{ csrf_field() }}
+<div class="grid is-100">
+    <form class="form" role="form" method="POST" action="{{ route('register') }}">
+        {{ csrf_field() }}
 
-            <div class="row">
-                {{ $errors->has('name') ? ' has-error' : '' }}
-                {{ $errors->has('email') ? ' has-error' : '' }}
-                {{ $errors->has('password') ? ' has-error' : '' }}
-                @if ($errors->has('name'))
-                {{ $errors->first('name') }}
-                @endif
-                @if ($errors->has('email'))
-                {{ $errors->first('email') }}
-                @endif
-                @if ($errors->has('password'))
-                {{ $errors->first('password') }}
-                @endif
-            </div>
+        <div class="form-item">
+            <a href="/redirect/facebook" class="login-with-facebook-container">
+                <icon name="facebook-square"></icon> Login with Facebook
+            </a>
+        </div>
 
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="user"></icon></span>
-                <input id="name" type="text" name="name" placeholder="Name" value="{{ old('name') }}" required autofocus>
-            </div>
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="envelope"></icon></span>
-                <input id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
-            </div>
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="lock"></icon></span>
-                <input id="password" type="password" name="password" placeholder="Password" required>
-            </div>
-            <div class="row icon-row">
-                <span class="input-field-icon"><icon name="lock"></icon></span>
-                <input id="password-confirm" type="password" name="password_confirmation" placeholder="Password" required>
-            </div>
-            <button type="submit" class="btn-primary">Register</button>
-        </form>
-    </div>
-    <ul class="mini-nav nav">
-        <li><a href="{{ route('login') }}">Login</a></li>
-    </ul>
+        <div class="form-item">
+            {{ $errors->has('name') ? ' has-error' : '' }}
+            {{ $errors->has('email') ? ' has-error' : '' }}
+            {{ $errors->has('password') ? ' has-error' : '' }}
+            @if ($errors->has('name'))
+            {{ $errors->first('name') }}
+            @endif
+            @if ($errors->has('email'))
+            {{ $errors->first('email') }}
+            @endif
+            @if ($errors->has('password'))
+            {{ $errors->first('password') }}
+            @endif
+        </div>
+
+        <div class="form-item">
+            <input id="name" type="text" name="name" placeholder="Name" value="{{ old('name') }}" required autofocus>
+        </div>
+        <div class="form-item">
+            <input id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+        </div>
+        <div class="form-item">
+            <input id="password" type="password" name="password" placeholder="Password" required>
+        </div>
+        <div class="form-item">
+            <input id="password-confirm" type="password" name="password_confirmation" placeholder="Password" required>
+        </div>
+        <button type="submit" class="btn-primary">Register</button>
+    </form>
 </div>
 @endsection

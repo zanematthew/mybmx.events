e1726b2ddb77ca6c10b1c6b20fc902ced16be1d3
Zane M. Kolnik <zane@zanematthew.com>
2017-12-11 22:04:32 -0500

    Elasticsearch queries, drafts.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,126 @@
 <?php
 
 Route::get('/test-search', function(){
+    // use Elasticsearch\ClientBuilder;
+    $client = \Elasticsearch\ClientBuilder::create()->build();
+
+    //
+    // Venue Proximity Search
+    //
+    $params = [
+        'index' => env('ELASTICSEARCH_INDEX'),
+        'type' => 'doc',
+            'body' => [
+            'query' => [
+                'bool' => [
+                    'must' => [ [ 'match_all' => new \stdClass() ] ],
+                    'filter' => [
+                        'geo_distance' => [
+                            'distance' => '200mi',
+                            'latlon' => '38.6364668,-77.2934339'
+                        ]
+                    ],
+                    'should' => [
+                        [ 'term' => ['z_type' => ['value' => 'venue' ] ] ]
+                    ],
+                    'minimum_should_match' => 1
+                ]
+            ],
+        ],
+    ];
+    $response = $client->search($params);
+    dd(collect($response));
+
+    //
+    // Venue text Search
+    //
+    $params = [
+        'index' => env('ELASTICSEARCH_INDEX'),
+        'type' => 'doc',
+            'body' => [
+            'query' => [
+                'bool' => [
+                    'must' => [
+                        ['term' => ['z_type' => [ 'value' => 'venue' ]]],
+                        ['match_phrase_prefix' => ['name' => 'mary']]
+                    ]
+                ]
+            ],
+        ],
+    ];
+    $response = $client->search($params);
+    dd(collect($response));
+
+
+// Events query
+// Text
+// Proximity
+// TextProximity
+//
+// Return;
+// Proximity 200 miles
+// Date range gte today
+//
+// Sorted by registration asc
+//
+
+    // 10 items
+    // Today + 30 days
+    // Sorted by closest to geo-point
+    // $params = [
+    //     'index' => env('ELASTICSEARCH_INDEX'),
+    //     'type' => 'doc',
+    //         'body' => [
+    //         'query' => [
+    //             'range' => [
+    //                 'registration' => [
+    //                     'gte'   => 'now+30d',
+    //                     'boost' => 2.0
+    //                 ]
+    //             ]
+    //         ],
+    //         'sort' => [
+    //             '_geo_distance' => [
+    //                 'latlon'        => '39.9670061,-76.7659088', // york, pa
+    //                 // 'latlon'        => '38.6364372,-77.2759241', // Woodbridge, VA
+    //                 'order'         => 'asc',
+    //                 'unit'          => 'km',
+    //                 'mode'          => 'min',
+    //                 'distance_type' => 'arc',
+    //             ]
+    //         ],
+    //         'size' => 10
+    //     ],
+    // ];
+    $params = [
+        'index' => env('ELASTICSEARCH_INDEX'),
+        'type' => 'doc',
+            'body' => [
+            'query' => [
+                'multi_match' => [
+                    'query' => 'Maryland',
+                    'fields' => ['state', 'city']
+                ]
+            ],
+            'sort' => [
+                '_geo_distance' => [
+                    'latlon'        => '39.9670061,-76.7659088', // york, pa
+                    // 'latlon'        => '38.6364372,-77.2759241', // Woodbridge, VA
+                    'order'         => 'asc',
+                    'unit'          => 'km',
+                    'mode'          => 'min',
+                    'distance_type' => 'arc',
+                ]
+            ],
+            'size' => 200
+        ],
+    ];
+
+    $response = $client->search($params);
+    // echo '<pre>';
+    // print_r(collect($response));
+    // die();
+
 // GET /test_index/_search
 // {
 //   "query" : {
@@ -30,76 +150,82 @@ Route::get('/test-search', function(){
 // 38.6364668,-77.2934339; // Woodbridge, VA
 // 39.2628271,-76.6350047; // MD
 
-    $r = App\Venue::search('', function ($engine, $query, $options) {
-        $options['body']['sort']['_geo_distance'] = [
-                'latlon'        => '39.2628271,-76.6350047',
-                'order'         => 'asc',
-                'unit'          => 'mi',
-                'mode'          => 'min',
-                'distance_type' => 'arc',
-        ];
-        return $engine->search($options);
-    })->where('z_type', 'venue')->take(10)->get()->pluck('name')->toArray();
-    dd($r);
+    // $r = App\Venue::search('', function ($engine, $query, $options) {
+    //     $options['body']['sort']['_geo_distance'] = [
+    //             'latlon'        => '39.2628271,-76.6350047',
+    //             'order'         => 'asc',
+    //             'unit'          => 'mi',
+    //             'mode'          => 'min',
+    //             'distance_type' => 'arc',
+    //     ];
+    //     return $engine->search($options);
+    // })->where('z_type', 'venue')->take(10)->get()->pluck('name')->toArray();
+    // dd($r);
 
-    $results = App\Venue::search('', function($engine, $query, $options) {
-        // $options['body']['query']['bool']['must'][] = [
-        //     'match_all' => new \stdClass()
-        // ];
-        $options['body']['sort']['_geo_distance'] = [
-            'latlon' => '39.2628271,-76.6350047', // VA
-            'order'         => 'asc',
-            'unit'          => 'km',
-            'mode'          => 'min',
-            'distance_type' => 'arc',
-        ];
-        return $engine->search($options);
-    })->take(300)->get()->pluck('name');
-    echo '<pre>';
-    print_r($results);
-    die();
+    // $results = App\Venue::search('', function($engine, $query, $options) {
+    //     $options['body']['query']['bool']['must'][] = [
+    //         'match_all' => new \stdClass()
+    //     ];
+    //     $options['body']['sort']['_geo_distance'] = [
+    //         'latlon' => '39.2628271,-76.6350047', // VA
+    //         'order'         => 'asc',
+    //         'unit'          => 'km',
+    //         'mode'          => 'min',
+    //         'distance_type' => 'arc',
+    //     ];
+    //     return $engine->search($options);
+    // })->take(30)->get()->pluck('name');
+    // echo '<pre>';
+    // print_r($results);
+    // die('this');
 
-    // Closest venues
-    $results = App\Venue::search('', function($engine, $query, $options) {
-        $options['body']['query']['bool']['filter'] = [
-            'range' => [
-                'registration' => [
-                    'gte'=> Illuminate\Support\Carbon::today()->toDateString(),
-                ]
-            ]
-        ];
-        $options['body']['sort']['_geo_distance'] = [
-            'latlon'        => '39.2628271,-76.6350047',
-            // 'latlon' => '38.6364668,-77.2934339', // VA
-            'order'         => 'asc',
-            'unit'          => 'km',
-            'mode'          => 'min',
-            'distance_type' => 'arc',
-        ];
-        return $engine->search($options);
-    })->take(20)->get()->load('venue.city.states')->toArray();
-    dd($results);
-
-    // Closets events; from this weekend
     $results = App\Event::search('', function($engine, $query, $options) {
         $options['body']['query']['bool']['filter'] = [
             'range' => [
                 'registration' => [
                     'gte'=> Illuminate\Support\Carbon::today()->toDateString(),
                 ]
+            ],
+            '_geo_distance' => [
+                'latlon'        => '39.2628271,-76.6350047',
+                // 'latlon' => '38.6364668,-77.2934339', // VA
+                'order'         => 'asc',
+                'unit'          => 'km',
+                'mode'          => 'min',
+                'distance_type' => 'arc',
             ]
         ];
-        $options['body']['sort']['_geo_distance'] = [
-            'latlon'        => '39.2628271,-76.6350047',
-            // 'latlon' => '38.6364668,-77.2934339', // VA
-            'order'         => 'asc',
-            'unit'          => 'km',
-            'mode'          => 'min',
-            'distance_type' => 'arc',
+        $options['body']['sort'] = [
+            'registration' => [
+                'order' => 'asc'
+            ]
         ];
         return $engine->search($options);
     })->take(20)->get()->load('venue.city.states')->toArray();
-    dd($results);
+    echo '<pre>';
+    print_r('this');
+    die();
+
+    // Closets events; from this weekend
+    // $results = App\Event::search('', function($engine, $query, $options) {
+    //     $options['body']['query']['bool']['filter'] = [
+    //         'range' => [
+    //             'registration' => [
+    //                 'gte'=> Illuminate\Support\Carbon::today()->toDateString(),
+    //             ]
+    //         ]
+    //     ];
+    //     $options['body']['sort']['_geo_distance'] = [
+    //         'latlon'        => '39.2628271,-76.6350047',
+    //         // 'latlon' => '38.6364668,-77.2934339', // VA
+    //         'order'         => 'asc',
+    //         'unit'          => 'km',
+    //         'mode'          => 'min',
+    //         'distance_type' => 'arc',
+    //     ];
+    //     return $engine->search($options);
+    // })->take(20)->get()->load('venue.city.states')->toArray();
+    // dd($results);
 
     // Keyword, sorted by closest;
     // $results = App\Venue::search('chester', function($engine, $query, $options) {
@@ -119,21 +245,21 @@ Route::get('/test-search', function(){
     // $foo = App\Venue::search('bmx')->take(200)->get()->pluck('name');
     // dd($foo);
 
-    // $results = App\Venue::search('', function($engine, $query, $options) {
-        // $options['body']['query']['bool']['filter']['geo_distance'] = [
-        //     'distance' => '500mi',
-        //     'latlon'   => '39.2628271,-76.6350047',
-        // ];
-        // $options['body']['sort']['_geo_distance'] = [
-        //     'latlon'        => '39.2628271,-76.6350047',
-        //     // 'latlon'        => '39.590010,-119.245782',
-        //     'order'         => 'asc',
-        //     'unit'          => 'km',
-        //     'mode'          => 'min',
-        //     'distance_type' => 'arc',
-        // ];
+    // $results = \App\Venue::search('', function($engine, $query, $options) {
+    //     $options['body']['query']['bool']['filter']['geo_distance'] = [
+    //         'distance' => '500mi',
+    //         'latlon'   => '39.2628271,-76.6350047',
+    //     ];
+    //     $options['body']['sort']['_geo_distance'] = [
+    //         'latlon'        => '39.2628271,-76.6350047',
+    //         // 'latlon'        => '39.590010,-119.245782',
+    //         'order'         => 'asc',
+    //         'unit'          => 'km',
+    //         'mode'          => 'min',
+    //         'distance_type' => 'arc',
+    //     ];
     //     return $engine->search($options);
-    // })->take(4)->get()->load('city.states')->pluck('name');
+    // })->take(400)->get()->load('city.states')->pluck('name');
     // dd($results);
 });
 
